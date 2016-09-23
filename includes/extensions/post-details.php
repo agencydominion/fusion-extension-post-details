d465ff7126ea535fd70666136a354995c2e77aac
Jeff Chew <jeff.chew@agencydominion.com>
2016-09-23 12:46:27 -0300

    move function definitions out from within methods

--- includes/extensions/post-details.php
+++ includes/extensions/post-details.php
@@ -11,53 +11,13 @@
  * @since 1.0.0
  */
 
+/**
+ * Map Shortcode
+ */
+
 add_action('init', 'fsn_init_post_details', 12);
 function fsn_init_post_details() {
  
- 	//OUTPUT SHORTCODE
- 	function fsn_post_details_shortcode( $atts ) {
-		extract( shortcode_atts( array(
-			'show_title' => '',
-			'show_author' => '',
-			'show_date' => '',
-			'show_categories' => '',
-			'show_tags' => ''
-		), $atts ) );
-		
-		global $post;
-		
-		$output = '<div class="fsn-post-details '. fsn_style_params_class($atts) .'">';
-			//prepend post details action hook
-	    	ob_start();
-	    	do_action('fsn_post_details_prepend', $post);
-	    	$output .= ob_get_clean();
-			$output .= !empty($show_title) ? apply_filters('fsn_post_details_title', '<h1 class="post-title">'. get_the_title($post) .'</h1>', $post) : '';
-			if (!empty($show_author) || !empty($show_date) || !empty($show_categories) || !empty($show_tags)) {
-				$author = !empty($show_author) ? true : false;
-				$date = !empty($show_date) ? true : false;
-				$categories = !empty($show_categories) ? true : false;
-				$tags = !empty($show_tags) ? true : false;
-				$args = array(
-					'author' => $author,
-					'date' => $date,
-					'categories' => $categories,
-					'tags' => $tags
-				);
-				$output .= '<footer class="post-metadata">';
-					$output .= fsn_get_post_meta($args);
-				$output .= '</footer>';
-			}
-			//append post details action hook
-	    	ob_start();
-	    	do_action('fsn_post_details_append', $post);
-	    	$output .= ob_get_clean();
-		$output .= '</div>';
-		
-		return $output;
-	}
-	add_shortcode('fsn_post_details', 'fsn_post_details_shortcode');
- 
-	//MAP SHORTCODE
 	if (function_exists('fsn_map')) {
 		fsn_map(array(
 			'name' => __('Post Details', 'fusion-extension-post-details'),
@@ -94,5 +54,51 @@ function fsn_init_post_details() {
 		));
 	}
 }
+
+/**
+ * Output Shortcode
+ */
+
+function fsn_post_details_shortcode( $atts ) {
+	extract( shortcode_atts( array(
+		'show_title' => '',
+		'show_author' => '',
+		'show_date' => '',
+		'show_categories' => '',
+		'show_tags' => ''
+	), $atts ) );
+	
+	global $post;
+	
+	$output = '<div class="fsn-post-details '. fsn_style_params_class($atts) .'">';
+		//prepend post details action hook
+    	ob_start();
+    	do_action('fsn_post_details_prepend', $post);
+    	$output .= ob_get_clean();
+		$output .= !empty($show_title) ? apply_filters('fsn_post_details_title', '<h1 class="post-title">'. get_the_title($post) .'</h1>', $post) : '';
+		if (!empty($show_author) || !empty($show_date) || !empty($show_categories) || !empty($show_tags)) {
+			$author = !empty($show_author) ? true : false;
+			$date = !empty($show_date) ? true : false;
+			$categories = !empty($show_categories) ? true : false;
+			$tags = !empty($show_tags) ? true : false;
+			$args = array(
+				'author' => $author,
+				'date' => $date,
+				'categories' => $categories,
+				'tags' => $tags
+			);
+			$output .= '<footer class="post-metadata">';
+				$output .= fsn_get_post_meta($args);
+			$output .= '</footer>';
+		}
+		//append post details action hook
+    	ob_start();
+    	do_action('fsn_post_details_append', $post);
+    	$output .= ob_get_clean();
+	$output .= '</div>';
+	
+	return $output;
+}
+add_shortcode('fsn_post_details', 'fsn_post_details_shortcode');
  
 ?>
