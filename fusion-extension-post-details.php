fde84b5ba1e56ead358a8028e7d3b65a97dbc37e
Jeff Chew <jeff.chew@agencydominion.com>
2016-09-26 15:06:21 -0300

    Update version number

--- fusion-extension-post-details.php
+++ fusion-extension-post-details.php
@@ -6,7 +6,7 @@
  * Plugin Name: Fusion : Extension - Post Details
  * Plugin URI: http://fusion.1867dev.com
  * Description: Post Details Extension Package for Fusion.
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Agency Dominion
  * Author URI: http://agencydominion.com
  * License: GPL2
